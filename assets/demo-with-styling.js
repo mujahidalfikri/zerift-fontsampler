0f1bc3f8e84e13cf811fa7d38ac78d42d4b5aa50
mujahidalfikri <115609712+mujahidalfikri@users.noreply.github.com>
2024-10-29 21:13:19 +0700

    Add files via upload

---
+++ assets/demo-with-styling.js
@@ -0,0 +1,41 @@
+/*
+ * A Fontsampler example using all UI elements
+ */
+window.addEventListener("load", function () {
+
+    var fonts = [
+        /*{
+            "name": "Alegreya Regular", 
+            "files": ["./fonts/alegreya/alegreya-v12-latin-regular.woff"],
+            "features": ["liga", "dlig"],
+            "language": "en-gb"
+        },*/ 
+        { 
+            "name": "Zerift", 
+            "files": ["./fonts/zerift/Zerift-Regular.woff"],
+            "features": ["liga", "dlig"],
+            "language": "en-gb"
+        }
+    ]
+
+    var options = {
+
+        multiline: true,
+
+        lazyload: true,
+
+        order: [
+            ["fontfamily", /*"alignment"*/],
+            ["fontsize", /*"lineheight", "opentype"*/],
+            "tester"
+        ],
+    }
+    
+    let nodes = document.querySelectorAll(".example-with-styling");
+
+    nodes.forEach(node => {
+        var fs = new Fontsampler(node, fonts, options)
+        FontsamplerSkin(fs)
+        fs.init()
+    })
+})
